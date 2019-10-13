febf3fcfef166640779a23e9d2c92a965708fa88
unknown <aziz.narkabilov@gmail.com>
2019-10-14 01:21:33 +0500

    guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.isGreater = true;
+        this.value = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.round((this.min + this.max)/2);
+        return this.value;
     }
 
     lower() {
-
+        this.isGreater = false;
+        this.max = this.value;
     }
 
     greater() {
-
+        this.isGreater = true;
+        this.min = this.value;
     }
 }
 
